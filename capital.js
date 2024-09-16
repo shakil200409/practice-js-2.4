91ecee9869a10e2a780c78647d1195896f12b5ea
Shakil Ahmed <shakil200409@gmail.com>
2024-09-16 23:19:54 +0600

    Capitalize Every first Letter of each word in a String

---
+++ capital.js
@@ -0,0 +1,9 @@
+const sentence = "A good job depands on your Experience X";
+function titleCase(str){
+    str = str.toLowerCase().split(" ");
+    for(let i= 0; i< str.length; i++){
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+    return str.join(" ");
+}
+console.log(titleCase(sentence));
